d26a9a978fea12a347d7cc220dc3932f13f2d923
AL-mamoura <yahyasailan343@gmail.com>
2025-05-06 16:01:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('enter-button').addEventListener('click', function() {
+  document.getElementById('splash').style.display = 'none';
+  document.getElementById('main-content').style.display = 'block';
+});
